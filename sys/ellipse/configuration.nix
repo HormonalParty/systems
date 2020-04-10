3c75a623359b5715a99ad5202d6f0c40c39d31b1
Danielle Lancashire <dani@builds.terrible.systems>
2020-04-10 11:32:24 +0000

    ellipse: migrate from endocrimes/system repo

---
+++ sys/ellipse/configuration.nix
@@ -0,0 +1,48 @@
+{ config, pkgs, lib, ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+    ../../modules/default
+    ../../modules/hardware/intel
+  ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.tmpOnTmpfs = true;
+
+  networking.hostName = "ellipse";
+  networking.hostId = "59adcdae";
+
+  networking.useDHCP = false;
+  networking.interfaces.enp3s0f0.useDHCP = true;
+
+  services.zfs.autoScrub.enable = true;
+  services.zfs.autoSnapshot = {
+    enable = true;
+    frequent = 8;
+    monthly = 3;
+  };
+
+  networking.firewall = {
+    enable = true;
+    allowPing = true;
+    allowedTCPPorts = [ 22 ];
+  };
+
+  services.plex = {
+    enable = true;
+    openFirewall = true;
+  };
+
+  fileSystems."/mnt/media" = {
+	  device = "//192.168.178.105/media";
+	  fsType = "cifs";
+	  options = let
+# this line prevents hanging on network split
+		  automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s,sec=ntlmssp";
+	  in ["${automount_opts},credentials=/etc/nixos/secrets-naspw,uid=${toString config.users.users.plex.uid},gid=${toString config.users.groups.plex.gid}"];
+  };
+
+  system.stateVersion = "20.03";
+}
