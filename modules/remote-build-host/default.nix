196cd038b170b1c554489a4f46215c0d0952ba0c
Maxine Aubrey <maxeaubrey@gmail.com>
2021-04-16 17:29:12 +0200

    m/remote-build-host: add chirm

--- modules/remote-build-host/default.nix
+++ modules/remote-build-host/default.nix
@@ -7,6 +7,7 @@
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMLG++puQtyfh0LVX8zu4KOtfgQFnJdbwT3JZZPsevam root@ellipse"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHMA2zzxDQEt2Nv9nNa6btvm/2fyVMXFSMyw1259+o0T root@mir"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHUSa0uFUgoaes/Z95LoijexaKfU9XeMB8GdQkvojWCj root@chirm"
     ];
   };
 }
