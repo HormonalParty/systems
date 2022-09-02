0320557bf91a9d56b373100165a3ad88219f6568
Maxine Aubrey <maxeaubrey@gmail.com>
2022-09-02 20:37:27 +0000

    m/plex: don't open firewall

--- modules/plex/default.nix
+++ modules/plex/default.nix
@@ -3,7 +3,18 @@
 {
   services.plex = {
     enable = true;
-    openFirewall = true;
+    openFirewall = false;
+    extraPlugins = [
+      (builtins.path {
+        name = "Audnexus.bundle";
+        path = pkgs.fetchFromGitHub {
+          owner = "djdembeck";
+          repo = "Audnexus.bundle";
+          rev = "v0.4.0";
+          sha256 = "sha256-9xNvOBmdWcOhTGLLs5fv6/5kq9eyocrWMkdpZanVO/s=";
+        };
+      })
+    ];
   };
 
   users.groups.media.members = [ "danielle" "maxine" "plex" ];
